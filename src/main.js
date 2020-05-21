c56e3b3c4267da8b9b7f082d08e21710e7e9eed4
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-05-21 06:46:16 +0200

    Added vuex to project

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import search from './components/Search.vue'
+import store from './store'
 
 Vue.config.productionTip = false
-Vue.component('home-search' , search);
+Vue.component('home-search', search);
 
 import router from './router'
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
