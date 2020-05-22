8952cb6f07c9262c36550d34fb57f1b945138870
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-05-22 08:57:27 +0200

    Set up Result.vue and minor stuff

---
+++ src/components/Result.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section id="results-container">
+      <!--- If user wants to make another search  --->
+      <input type="text" />
+      <input type="text" />
+      <input type="button" />
+      <!--- filter options here --->
+      <div class="results-object"></div>
+    </section>
+  </div>
+</template>
+
+<script>
+/**
+ * TO-DO
+ * Get results from GET-request from database (which should be stored in store) and
+ *  populate elements in this template.
+ * Link inputs to from and to in store as two-way computed component.
+ * Use this component in views/Searchresult
+ */
+export default {};
+</script>
+
+<style>
+</style>
