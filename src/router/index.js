82e1a15ff82d62d715edc352024f3fdadfc38f8f
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-05-24 20:35:32 +0200

    merge with putte

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Store from '../store'
 Vue.use(VueRouter)
 
 //Importing views
-import Login from '../views/Login'
+//import Login from '../views/Login'
 import Home from '../views/Home'
 import Register from '../views/Register'
 import Searchresult from '../views/Searchresult'
@@ -18,9 +18,6 @@ let router = new VueRouter({
     component: Home,
     path: '/'
   }, {
-    component: Login,
-    path: '/login'
-  }, {
     name: "Register",
     component: Register,
     path: '/register'
@@ -29,11 +26,11 @@ let router = new VueRouter({
     name: "Searchresult",
     component: Searchresult,
     path: '/results'
-  },{
+  }, {
     name: "User",
     component: User,
     path: '/user',
-    meta:{
+    meta: {
       requiresAuth: true
     }
   }, {
@@ -45,17 +42,17 @@ let router = new VueRouter({
 
 //Avoid navigating to Users if not logged in
 router.beforeEach((to, from, next) => {
-  if(to.meta.requiresAuth){
+  if (to.meta.requiresAuth) {
     //Check if user is logged in
-    if(Store.state.loggedIn){
+    if (Store.state.loggedIn) {
       next()
     }
     //Else, push them back to Homescreen.
-    else{
-      router.push({name:"Home"})
+    else {
+      router.push({ name: "Home" })
     }
   }
-  else{
+  else {
     next()
   }
 })
