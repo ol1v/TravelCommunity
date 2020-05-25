01f37cbe1ef0054f29b2bd491b9c25ffef616c41
muhannadsayedabdullah <symohannad@gmail.com>
2020-05-25 16:06:03 +0200

    Footer added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="header">
     <home-search></home-search>
+    <MyFooter></MyFooter>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-#header{
+#header {
   width: 100%;
   height: calc(100vh - 50px);
-  background-image: url('../assets/header.jpg');
+  background-image: url("../assets/header.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   margin: 0px;
+  margin-bottom: -50px;
   overflow: hidden;
   padding: 0px;
 }
-
 </style>
