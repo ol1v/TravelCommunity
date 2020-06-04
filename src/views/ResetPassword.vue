dadebc7a5c3f0d2c096eaa4c4be13140817c8c0d
PutteAbrahamsson <puttemaden98@hotmail.com>
2020-06-04 10:52:09 +0200

    Responsive design for 'Forgot password' is updated.

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div id="ban-user-wrapper">
-      <h2 class="center">Återställ lösenord</h2>
-      <label for="user">Email</label>
-      <input type="email" id="email" class="ban-user-input" v-model="email">
-      <input type="button" value="Återställ lösenord" class="ban-user-button" @click="resetPassword">
+    <div id="reset-pass-wrapper">
+      <h2 class="center font">Återställ lösenord</h2>
+      <label for="email" class="font">Email</label>
+      <input type="email" id="email" class="reset-pass-input" v-model="email">
+      <input type="button" value="Återställ lösenord" class="reset-pass-button font" @click="resetPassword">
     </div>
   </div>
 </template>
@@ -39,27 +39,49 @@ export default {
 }
 </script>
 
-<style>
-#ban-user-wrapper{
+<style scoped>
+#reset-pass-wrapper{
   width: 15%;
   height: auto;
   padding: 10px;
   margin: 50px auto;
 }
-.ban-user-input{
+.reset-pass-input{
   width: 100%;
   height: 30px;
   box-sizing: border-box;
 }
-.ban-user-button{
+.reset-pass-button{
   width: 100%;
   height: 40px;
   margin-top: 10px;
   cursor: pointer;
+  background-color: #026f7e;
+  color: white;
+}
+
+@media (min-width: 769px) and (max-width: 1500px) {
+  #reset-pass-wrapper{
+    width: 20%;
+    height: auto;
+    padding: 10px;
+    margin: 50px auto;
+    border-radius: 0px;
+  }
+}
+
+@media (min-width: 501px) and (max-width: 768px) {
+  #reset-pass-wrapper{
+    width: 80%;
+    height: auto;
+    padding: 10px;
+    margin: 50px auto;
+    border-radius: 0px;
+  }
 }
 
 @media (max-width: 500px) {
-  #ban-user-wrapper{
+  #reset-pass-wrapper{
     width: 80%;
     height: auto;
     padding: 10px;
